07bc885ad040d3a9966070dbc02cb68820b94c4c
Anta Osotsi <aosotsi@gmail.com>
2023-07-14 16:44:17 -0700

    Printing nums

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Description: prints all numbers from 0 to 9
+ * Return: 0
+ */
+
+int main(void)
+{
+  int a;
+  for (a = 0; a <10; a++)
+    printf("%d",a);
+
+  putchar('\n');
+
+  return (0);
+}
