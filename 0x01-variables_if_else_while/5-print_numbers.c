92bba6fe22f1ad12648b5f090254e137d340c464
Anta Osotsi <aosotsi@gmail.com>
2023-07-14 16:50:29 -0700

    Printing nums

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,11 +8,12 @@
 
 int main(void)
 {
-  int a;
-  for (a = 0; a <10; a++)
-    printf("%d",a);
+	int a;
 
-  putchar('\n');
+	for (a = 0; a <10; a++)
+		printf("%d",a);
 
-  return (0);
+	putchar('\n');
+
+	return (0);
 }
