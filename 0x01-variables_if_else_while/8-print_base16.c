d3af3e5a2e6d3451de03f982946722be8c779e2e
Anta Osotsi <aosotsi@gmail.com>
2023-07-14 17:09:16 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Description: Print all numbers of hex in lowercase.
+ * 
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+	}
+	for (c = 'a'; c < 'g'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
