75fcea6eaca234f48916e95ce2dee435353f331a
Anta Osotsi <aosotsi@gmail.com>
2023-07-19 04:35:29 +0300

    five and six

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_abs - Check Main
+ * @r: An integer input
+ * Description: This function returns the abs  of a number
+ * Return: Absolut value of number r
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
