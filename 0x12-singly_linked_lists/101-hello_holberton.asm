38296170db537018c58c8fb7c724699bdb0f3f34
Anta Osotsi <aosotsi@gmail.com>
2023-08-24 23:18:17 +0300

    Fixing bonus

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,25 +1,12 @@
-section .data
-    hello db "Hello, Holberton", 0
-    format db "%s", 10, 0  ; "%s" for string, 10 for newline, 0 to terminate the string
+global main
+extern printf
 
-section .text
-    global main
-    extern printf
-
+section.text
 main:
-    ; Push the address of the format string onto the stack
-    lea rdi, [format]
-    
-    ; Push the address of the hello string onto the stack
-    lea rsi, [hello]
-    
-    ; Call printf
-    call printf
-    
-    ; Clean up the stack
-    add rsp, 8
-    
-    ; Exit the program
-    mov rax, 60         ; syscall number for exit
-    xor rdi, rdi        ; status 0
-    syscall
+mov rdi, fmt
+mov rax, 0
+call    printf
+mov rax, 0
+ret
+fmt:
+    db "Hello, Holberton", 10, 0
