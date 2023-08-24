0e3e6039a907c002411634f24fa39460e6a870cd
Anta Osotsi <aosotsi@gmail.com>
2023-08-24 23:15:21 +0300

    Assembly fix

--- 0x12-singly_linked_lists/101-hello_school.asm
+++ 0x12-singly_linked_lists/101-hello_school.asm
@@ -1,12 +1,25 @@
-global main
-extern printf
+section .data
+    hello db "Hello, Holberton", 0
+    format db "%s", 10, 0  ; "%s" for string, 10 for newline, 0 to terminate the string
 
-section.text
+section .text
+    global main
+    extern printf
+
 main:
-mov	rdi, fmt
-mov	rax, 0
-call	printf
-mov	rax, 0
-ret
-fmt:
-	db "Hello, Holberton", 10, 0
+    ; Push the address of the format string onto the stack
+    lea rdi, [format]
+    
+    ; Push the address of the hello string onto the stack
+    lea rsi, [hello]
+    
+    ; Call printf
+    call printf
+    
+    ; Clean up the stack
+    add rsp, 8
+    
+    ; Exit the program
+    mov rax, 60         ; syscall number for exit
+    xor rdi, rdi        ; status 0
+    syscall
